e5d9750852168765ac213b667164f6ceeb527a58
olga <olga.shustikova1986@gmail.com>
2023-08-31 14:40:00 -0400

    Initial commit

---
+++ src/test/java/webserviceworkingday/testwebserviceworkingday/TestWebServiceWorkingDayApplicationTests.java
@@ -0,0 +1,13 @@
+package webserviceworkingday.testwebserviceworkingday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebServiceWorkingDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
